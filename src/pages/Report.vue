e495b8b216db98476aefeffdf8bb3fadc2288190
Markrodriguez1105 <markanthonyrodriguez59@gmail.com>
2024-06-03 19:22:30 +0800

    6/3/2024

--- src/pages/Report.vue
+++ src/pages/Report.vue
@@ -1,5 +1,31 @@
 <template>
     <div>
-        <h1>Report</h1>
+      <h1 class="centered-heading">
+        <span class="line"></span>
+        Report
+        <span class="line"></span>
+      </h1>
+      <RequestReport />
     </div>
-</template>
+  </template>
+  
+  <script setup>
+  import RequestReport from '@/components/Report/RequestReport.vue'
+  </script>
+  
+  <style scoped>
+  .centered-heading {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    margin: 0 35px;
+  }
+  
+  .line {
+    flex: 1;
+    height: 3px;
+    background-color: black; 
+  }
+  </style>
+  
